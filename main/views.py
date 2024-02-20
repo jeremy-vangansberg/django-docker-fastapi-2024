e1b7d2408bba996668f241ef12ee74de3678716a
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2024-02-20 12:35:53 +0100

    api view wip

--- main/views.py
+++ main/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from .utils import mutliplicate_by_5
 from django.contrib.auth.decorators import login_required
+from .forms import CryptoApiForm
+
+@login_required
+def api_page(request):
+        # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = CryptoApiForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+
+    else:
+        form = CryptoApiForm()
+
+    return render(request, "main/api_page.html", context={'form':form})
+
+
 
 # Create your views here.
 def home_page(request):
@@ -17,6 +36,3 @@ def contact_page(request, test):
 def special_page(request):
     return render(request, "main/special_page.html")
 
-@login_required
-def api_page(request):
-    return render(request, "main/api_page.html")
