5f6684a00ae4082460e24592b7f1e6ba19dfc6d7
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2024-02-22 14:47:39 +0100

    entrypoint

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -3,5 +3,4 @@
 # Effectuer les migrations
 echo "Effectuer les migrations de la base de données..."
 python manage.py makemigrations && python manage.py migrate
-
-python manage.py runserver
+python manage.py runserver 0.0.0.0:8000
